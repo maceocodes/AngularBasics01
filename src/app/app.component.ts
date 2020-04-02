4c6bbb9e2e1ca2e22a09f0f31d25cd5e6bc535ac
maceo lewis <maceo.code@gmail.com>
2020-04-01 23:36:59 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Basics 01';
+  title:string = 'Angular Basics 01';
+
+  constructor() {
+    this.changeTitle('new title2');
+  }
+
+  changeTitle(title:string){
+    this.title = title;
+  }
 }
